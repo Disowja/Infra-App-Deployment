4b35b62367d33fb6c9534a8f05044b71395c03e1
@D'souza Meka <78405170+Disowja@users.noreply.github.com>
2024-08-19 00:24:37 +0530

    Update targetgroup.tf

--- Infra/tf-modules/tf-targetgroup/targetgroup.tf
+++ Infra/tf-modules/tf-targetgroup/targetgroup.tf
@@ -12,18 +12,18 @@ resource "aws_lb_target_group" "ecs-alb-target-group" {
 
 resource "aws_lb_listener_rule" "alb_listener_rule" {
   listener_arn = var.listenerArn
-  #priority     = var.priorityNumber
+  priority     = var.priorityNumber
 
   action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.ecs-alb-target-group.arn
   }
 
-  #condition {
-   # path_pattern {
-    #  values = [var.pathName]
-    #}
-  #}
+  condition {
+    path_pattern {
+      values = [var.pathName]
+    }
+  }
 }
 
 output "target_group_arn" {
